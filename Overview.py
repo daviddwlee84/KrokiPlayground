5e5946797da47362ad8c41fe33e1a79becf7d127
Da-Wei Lee <daviddwlee84@gmail.com>
2023-12-29 17:04:49 +0800

    Able to use manual input and add Mermaid support

--- Overview.py
+++ Overview.py
@@ -8,6 +8,8 @@ from io import BytesIO
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
+st.session_state.kroki_endpoint = st.text_input("Kroki Endpoint", "https://kroki.io/")
+
 """
 # Kroki Playground
 """
@@ -18,29 +20,44 @@ with st.expander("Kroki Cheatsheet"):
         caption="Kroki Cheatsheet",
     )
 
-example_file = st.selectbox(
-    "Example",
-    glob(os.path.join(curr_dir, "examples/*/*")),
-    format_func=lambda x: f"{os.path.basename(os.path.dirname(x))}/{os.path.basename(x)}",
-)
-# https://stackoverflow.com/questions/541390/extracting-extension-from-filename-in-python
-extension = os.path.splitext(example_file)[1].strip(".")
+mode: Literal["From Demo", "Manual"] = st.selectbox("Mode", ["From Demo", "Manual"])
 
-st.session_state.kroki_endpoint = st.text_input("Kroki Endpoint", "https://kroki.io/")
-image_type: Literal["svg", "png"] = st.selectbox(
-    "Image Type", config.extension_image_support[extension]
-)
-st.caption("Note that: svg is always supported.")
+if mode == "From Demo":
+    example_file = st.selectbox(
+        "Example",
+        glob(os.path.join(curr_dir, "examples/*/*")),
+        format_func=lambda x: f"{os.path.basename(os.path.dirname(x))}/{os.path.basename(x)}",
+    )
+    # https://stackoverflow.com/questions/541390/extracting-extension-from-filename-in-python
+    extension = os.path.splitext(example_file)[1].strip(".")
+    graph_type = config.extension_compiler_map[extension]
+
+    image_type: Literal["svg", "png", "jpeg", "pdf", "txt", "base64"] = st.selectbox(
+        "Image Type", config.extension_image_support[extension]
+    )
+    st.caption("Note that: svg is always supported.")
+
+    with open(example_file, "r") as fp:
+        example = fp.read()
+
+else:
+    graph_type: Literal["GraphViz", "Mermaid"] = st.selectbox(
+        "Graph Type", ["GraphViz", "Mermaid"]
+    )
+
+    image_type: Literal["svg", "png", "jpeg", "pdf", "txt", "base64"] = st.selectbox(
+        "Image Type", config.graph_image_support[graph_type]
+    )
+
+    example = "digraph G {Hello->World}"
+
 
 code_editor = utils.get_customized_code_editor()
-with open(example_file, "r") as fp:
-    example = fp.read()
-
-result_value = code_editor(example, lang=extension)
+result_value = code_editor(example, lang=graph_type)
 
 kroki_config = {
     "kroki_endpoint": st.session_state.kroki_endpoint,
-    "graph_type": config.extension_compiler_map[extension],
+    "graph_type": graph_type.lower(),
     "image_type": image_type,
     "content": result_value.get("text"),
     "encode": utils.encoder(result_value.get("text")),
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # Make this a YAML config file..?
-extension_compiler_map = {"dot": "graphviz"}
+extension_compiler_map = {"dot": "GraphViz"}
 # https://kroki.io/#support
 extension_image_support = {"dot": ["svg", "png"]}
+graph_image_support = {"GraphViz": ["svg", "png"], "Mermaid": ["svg", "png"]}
